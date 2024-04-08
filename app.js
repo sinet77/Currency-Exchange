619f3ea827719939fd3e8bad673611a345132492
Pawel <p.awel608@wp.pl>
2024-04-08 19:02:46 +0200

    commit

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const exchangeButton = document.getElementById('exchange')
 
 
 async function fetchDataCurrency() {
-    const url = 'https://currency-exchange.p.rapidapi.com/listquotes';
+    const url = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_OZc8C0HqZZkVDcx79ZnmqTpKV72UKa3dqjz1jT7g';
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '37dd2f3585mshc686436de66ad7bp1b621fjsne2272199deb7',
-            'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
+            'X-RapidAPI-Key': 'cur_live_OZc8C0HqZZkVDcx79ZnmqTpKV72UKa3dqjz1jT7g',
         }
     };
 
@@ -22,35 +21,27 @@ async function fetchDataCurrency() {
 
 }
 
-async function exchange(from, to, quantity) {
-    const url = `https://currency-exchange.p.rapidapi.com/exchange?from=${from}&to=${to}&q=${quantity}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '37dd2f3585mshc686436de66ad7bp1b621fjsne2272199deb7',
-            'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-        }
-    };
-
-
-    const response = await fetch(url, options);
-    const result = await response.text();
-
-    return result;
-
-}
 
 async function app() {
 
     const currencies = await fetchDataCurrency()
 
-    const currenciesArray = currencies.split(',')
-    console.log(currenciesArray)
+    const currencyCodes = [];
+
+
+
+    for (const currency in currencies) {
+        if (currencies.hasOwnProperty(currency)) {
+            currencyCodes.push(currencies[currency].code);
+        }
+    }
+    console.log(currencyCodes);
+
 
     const fromCurrency = document.getElementById('fromCurrency')
     const toCurrency = document.getElementById('toCurrency')
 
-    currenciesArray.forEach(part => {
+    currencyCodes.forEach(part => {
 
 
         const option = document.createElement('option')
@@ -61,7 +52,7 @@ async function app() {
         const option2 = document.createElement('option')
         option2.textContent = part;
         option2.value = part;
-        toCurrency.appendChild(option2)   /// nie mozna tak na 2 opcje dla forEach
+        toCurrency.appendChild(option2)
 
     })
 
